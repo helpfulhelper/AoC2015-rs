b1201937197587617c9273922c478a359c074f25
HelpfulHelper <16364535+helpfulhelper@users.noreply.github.com>
2022-05-02 19:54:47 -0400

    day4 - yay for lots of examples on the net.

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,7 +1,22 @@
-use std::fs;
+extern crate crypto;
+use crypto::digest::Digest;
+use crypto::md5::Md5;
 
 fn main() {
-    let filename: &str = "./src/bin/day01.txt";
-    let data = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    println!("{}", data);
+    let input = "bgvyzdsv".as_bytes();
+    let mut hasher = Md5::new();
+    for i in (0..std::u64::MAX) {
+        hasher.input(input);
+        hasher.input(i.to_string().as_bytes());
+
+        let mut output = [0; 16];
+        hasher.result(&mut output);
+
+        let first_five = output[0] as i32 + output[1] as i32 + (output[2] >> 4) as i32;
+        if first_five == 0 {
+            println!("{}", i);
+            break;
+        }
+        hasher.reset();
+    }
 }
