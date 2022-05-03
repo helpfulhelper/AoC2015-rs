6f33363c236f9e55a705c6dc6ce55c1a2fb98d93
HelpfulHelper <16364535+helpfulhelper@users.noreply.github.com>
2022-05-02 21:24:26 -0400

    didn't like the way it worked earlier.  this is much clearer to me. also day4b completed.

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -5,15 +5,15 @@ use crypto::md5::Md5;
 fn main() {
     let input = "bgvyzdsv".as_bytes();
     let mut hasher = Md5::new();
-    for i in (0..std::u64::MAX) {
+    for i in 0..std::u64::MAX {
         hasher.input(input);
         hasher.input(i.to_string().as_bytes());
 
         let mut output = [0; 16];
         hasher.result(&mut output);
 
-        let first_five = output[0] as i32 + output[1] as i32 + (output[2] >> 4) as i32;
-        if first_five == 0 {
+        let s = String::from_iter(output.iter().map(|b| format!("{:02X}", b)));
+        if s.starts_with("000000") {
             println!("{}", i);
             break;
         }
