da690d58219db6de2ff3f9f3423a2819f22153ab
HelpfulHelper <16364535+helpfulhelper@users.noreply.github.com>
2022-05-03 21:18:09 -0400

    ugh, part b was harder. kept screwing up chunking

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -5,7 +5,7 @@ fn main() {
     let data = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let mut count = 0;
     for d in data.lines() {
-        if nice(d) {
+        if nice2(d) {
             count += 1;
         }
     }
@@ -13,12 +13,35 @@ fn main() {
 }
 
 fn nice(s: &str) -> bool {
-    !tabboo(s) && vows(s) && dubs(s)
+    !tabboo(s) && vowels(s) && dubs(s, 1)
 }
 
-fn dubs(s: &str) -> bool {
+fn nice2(s: &str) -> bool {
+    dubs(s, 2) && pairs(s)
+}
+
+fn pairs(s: &str) -> bool {
+    let mut z = s.chars().peekable();
+    while z.peek().is_some() {
+        let chunk: String = z.by_ref().take(2).collect();
+        if chunk.len() == 2 && s.split(&chunk).count() - 1 > 1 {
+            return true;
+        };
+    }
+    z = s.chars().peekable();
+    let _z: String = z.by_ref().take(1).collect();
+    while z.peek().is_some() {
+        let chunk: String = z.by_ref().take(2).collect();
+        if chunk.len() == 2 && s.split(&chunk).count() - 1 > 1 {
+            return true;
+        };
+    }
+    false
+}
+
+fn dubs(s: &str, skip: usize) -> bool {
     let mut counter = 0;
-    for (i, j) in s.chars().zip(s.chars().skip(1)) {
+    for (i, j) in s.chars().zip(s.chars().skip(skip)) {
         if i == j {
             counter += 1;
         }
@@ -26,7 +49,7 @@ fn dubs(s: &str) -> bool {
     counter >= 1
 }
 
-fn vows(s: &str) -> bool {
+fn vowels(s: &str) -> bool {
     let mut counter = 0;
     for i in "aeiou".chars() {
         let count = s.matches(i).count();
@@ -34,15 +57,6 @@ fn vows(s: &str) -> bool {
     }
     counter >= 3
 }
-
-// fn tabboo(s: &str) -> bool {
-//     //if s.find("ab" || "cd" || "pq" || "xy") {
-//     if s.contains("ab") || s.contains("cd") || s.contains("pq") || s.contains("xy") {
-//         true
-//     } else {
-//         false
-//     }
-// }
 
 fn tabboo(s: &str) -> bool {
     let bad = ["ab", "cd", "pq", "xy"];
@@ -52,4 +66,20 @@ fn tabboo(s: &str) -> bool {
         }
     }
     false
+}
+
+#[test]
+fn test_dubs() {
+    assert_eq!(dubs("abcdefgg", 1), true);
+    assert_eq!(dubs("abcdefgh", 1), false);
+    assert_eq!(dubs("abaca", 2), true);
+    assert_eq!(dubs("abaca", 2), true);
+}
+
+#[test]
+fn test_dubdubs() {
+    assert_eq!(pairs("aaa"), false);
+    assert_eq!(pairs("aabaa"), true);
+    assert_eq!(pairs("aaaa"), true);
+    assert_eq!(pairs("ieodomkazucvgmuy"), false);
 }
